e58160416402db486307b1cb748f026024da597b
Anjali Pathak <87489165+PathakAnjali@users.noreply.github.com>
2022-09-07 18:03:53 +0530

    Update WaterMarked_Pdf.py

--- WaterMarked_Pdf.py
+++ WaterMarked_Pdf.py
@@ -1,14 +1,14 @@
 
 import PyPDF2
 
-MergePdf_Input = PyPDF2.PdfFileReader(open('Merged.pdf', 'rb'))
-Wtr_Input = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+MergePdf_Input = PyPDF2.PdfFileReader(open('Merged.pdf', 'rb'))         # add pdf to be watermarked
+Wtr_Input = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))                 # add pdf from which to watermarked other pdf
 Output = PyPDF2.PdfFileWriter()
 
 # looping  through the pfd to be watermarked
-for pdf in range(MergePdf_Input.getNumPages()):     # unaware of the no. of pgs in pdf so using getNumPages
+for pdf in range(MergePdf_Input.getNumPages()):   
     pages = MergePdf_Input.getPage(pdf)
-    pages.mergePage(Wtr_Input.getPage(0))             # as Wtr_Input have only one page, merging wtr mark pdf on all the pages
+    pages.mergePage(Wtr_Input.getPage(0))            
     Output.addPage(pages)
 
     with open('WaterMarked.pdf', 'wb') as file:
